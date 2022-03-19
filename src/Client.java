3d354004d6cff23815116e32f024315066b116a1
Бродников Денис <platz198@yandex.ru>
2022-03-19 12:45:14 +0700

    first commit

---
+++ src/Client.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+
+        final String host = "netology.homework";
+
+        int port = 8080;
+
+        Socket clientSocket = new Socket(host, port);
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        out.println("Host: " + host);
+
+        String resp = in.readLine();
+        System.out.println(resp);
+
+        String resp2 = in.readLine();
+        System.out.println(resp2);
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        out.println(name);
+
+        String resp3 = in.readLine();
+        System.out.println(resp3);
+
+        String answer = scanner.nextLine();
+        out.println(answer);
+
+        String resp4 = in.readLine();
+        System.out.println(resp4);
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+        int port = 8080;
+
+        System.out.println("Server startup");
+
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        System.out.println("New connection accepted");
+        final String host = in.readLine();
+        out.println(String.format("Hi %s, your port is %d \nWrite your name", host, clientSocket.getPort()));
+        final String username = in.readLine();
+
+        out.println("Are you child? (yes/no)");
+        final String answer = in.readLine();
+        if (answer.equals("yes")) {
+            out.println("Welcome to the kids area, " + username + "! Let's play!");
+        } else if (answer.equals("no")) {
+            out.println("Welcome to the adult zone, " + username + "! Have a good rest, or a good working day!");
+        }
+        serverSocket.close();
+    }
+}
